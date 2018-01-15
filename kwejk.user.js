34c2cc1b1225da2668e35ea4e73640bd93c4dd2f
Jeremiasz Mazur <Cross77@users.noreply.github.com>
2018-01-15 01:09:32 +0100

    Create kwejk.user.js

---
+++ kwejk.user.js
@@ -0,0 +1,13 @@
+/* The MIT License (MIT) */
+// ==UserScript==
+// @name         ANTI ANTI ADBLOCK
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  anti anti adblock
+// @author       https://github.com/Cross77
+// @include      https://kwejk.pl/*
+// @grant        none
+// ==/UserScript==
+
+Common.adb.notDetected();
+Common.adb = null;
