a90360fdebb23d336ff65e12f4297b5995485f2b
Jeremiasz Mazur <Cross77@users.noreply.github.com>
2018-01-15 01:20:50 +0100

    Update kwejk.user.js

--- kwejk.user.js
+++ kwejk.user.js
@@ -1,9 +1,9 @@
 /* The MIT License (MIT) */
 // ==UserScript==
-// @name         ANTI ANTI ADBLOCK
+// @name         ANTI ANTI ADBLOCK kwejk.pl
 // @namespace    http://tampermonkey.net/
 // @version      1.0
-// @description  anti anti adblock
+// @description  anti anti adblock kwejk.pl website
 // @author       https://github.com/Cross77
 // @include      https://kwejk.pl/*
 // @grant        none
