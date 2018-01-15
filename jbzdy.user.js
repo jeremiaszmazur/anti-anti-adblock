ee0387ce6e6516e7e3a97f026c88fec17f6e8811
Jeremiasz Mazur <Cross77@users.noreply.github.com>
2018-01-15 01:20:33 +0100

    Update jbzdy.user.js

--- jbzdy.user.js
+++ jbzdy.user.js
@@ -1,9 +1,9 @@
 /* The MIT License (MIT) */
 // ==UserScript==
-// @name         ANTI ANTI ADBLOCK
+// @name         ANTI ANTI ADBLOCK jbzdy.pl
 // @namespace    http://tampermonkey.net/
 // @version      1.0
-// @description  anti anti adblock
+// @description  anti anti adblock jbzdy.pl website
 // @author       https://github.com/Cross77
 // @include      https://jbzdy.pl/*
 // @grant        none
