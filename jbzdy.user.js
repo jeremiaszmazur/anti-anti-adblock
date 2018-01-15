c48ce0e71a83733f2b5bd6db8370689b5e665984
Jeremiasz Mazur <Cross77@users.noreply.github.com>
2018-01-15 01:09:01 +0100

    Create jbzdy.user.js

---
+++ jbzdy.user.js
@@ -0,0 +1,13 @@
+/* The MIT License (MIT) */
+// ==UserScript==
+// @name         ANTI ANTI ADBLOCK
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  anti anti adblock
+// @author       https://github.com/Cross77
+// @include      https://jbzdy.pl/*
+// @grant        none
+// ==/UserScript==
+
+App.adb.notDetected();
+App.adb = null;
